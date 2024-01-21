7f109beab56f2255568c80fa262074bfde15db90
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-01-20 23:29:54 -0600

    set local storage for inputs and set a function to update the time intervals

--- script.js
+++ script.js
@@ -7,14 +7,72 @@ $(function () {
   var currentDateTime = dayjs();
   var displayDate = currentDateTime.format("MMMM D, YYYY");
   var currentHour = currentDateTime.format("H");
-  console.log(displayDate);
+  var currentTime = currentDateTime.format("Hmm");
+
+  function updateTime() {
+    displayDate = currentDateTime.format("MMMM D, YYYY");
+    currentHour = currentDateTime.format("H");
+    currentTime = currentDateTime.format("Hmm");
+    if (currentTime == "00") {
+      localStorage.clear();
+    }
+  }
+
+  setInterval(updateTime, 1000);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  var hour9 = document.getElementById("hour-9");
+  var hour10 = document.getElementById("hour-10");
+  var hour11 = document.getElementById("hour-11");
+  var hour12 = document.getElementById("hour-12");
+  var hour13 = document.getElementById("hour-13");
+  var hour14 = document.getElementById("hour-14");
+  var hour15 = document.getElementById("hour-15");
+  var hour16 = document.getElementById("hour-16");
+  var hour17 = document.getElementById("hour-17");
+
+  hour9.children[2].addEventListener("click", function () {
+    var userIput9 = hour9.children[1].value;
+    localStorage.setItem("hour9", userIput9);
+  });
+  hour10.children[2].addEventListener("click", function () {
+    var userInput10 = hour10.children[1].value;
+    localStorage.setItem("hour10", userInput10);
+  });
+  hour11.children[2].addEventListener("click", function () {
+    var userInput11 = hour11.children[1].value;
+    localStorage.setItem("hour11", userInput11);
+  });
+  hour12.children[2].addEventListener("click", function () {
+    var userInput12 = hour12.children[1].value;
+    localStorage.setItem("hour12", userInput12);
+  });
+  hour13.children[2].addEventListener("click", function () {
+    var userInput13 = hour13.children[1].value;
+    localStorage.setItem("hour13", userInput13);
+  });
+  hour14.children[2].addEventListener("click", function () {
+    var userInput14 = hour14.children[1].value;
+    localStorage.setItem("hour14", userInput14);
+  });
+  hour15.children[2].addEventListener("click", function () {
+    var userInput15 = hour15.children[1].value;
+    localStorage.setItem("hour15", userInput15);
+  });
+  hour16.children[2].addEventListener("click", function () {
+    var userInput16 = hour16.children[1].value;
+    localStorage.setItem("hour16", userInput16);
+  });
+  hour17.children[2].addEventListener("click", function () {
+    var userInput17 = hour17.children[1].value;
+    localStorage.setItem("hour17", userInput17);
+  });
+
   // TODO: COMPLETE Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
